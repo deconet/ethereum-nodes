ef595a016f95497e1d6a64e336ec259c5ec08c71
Chris Cassano <glitch0@gmail.com>
2018-03-28 14:33:24 -0700

    added rpc port to ropsten

--- ropsten/start.sh
+++ ropsten/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8545:8545 -p 30303:30303 -it -v /root/geth-ropsten:/root/.ethereum ethereum/client-go --fast --testnet --cache 16 --rpc --rpcapi eth,net,web3,debug --rpcaddr 0.0.0.0 --rpccorsdomain "*" --maxpeers 128
+docker run -d -p 8545:8545 -p 30303:30303 -it -v /root/geth-ropsten:/root/.ethereum ethereum/client-go --fast --testnet --cache 16 --rpc --rpcport 8545 --rpcapi eth,net,web3,debug --rpcaddr 0.0.0.0 --rpccorsdomain "*" --maxpeers 128 
